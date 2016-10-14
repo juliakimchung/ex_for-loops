f909a9f2e2fe6de1ca1f32b7ce69ae1f4b2eb580
juliakimchung <julhkm@gmail.com>
2016-10-14 10:34:54 -0500

    finished

---
+++ for-loops.js
@@ -0,0 +1,42 @@
+var counter = i;
+
+for (var i = 5; i < 120; i+10) {
+console.log("conter value", counter);
+}
+
+var counter = i;
+
+for (var i = 4096; i >= 1; 2/i) {
+	console.log("counter value:", counter);
+}
+
+var arr =["George Washington", "John Adams", "Thomas Jefferson"];
+ for (var i = 0; i < arr.length; i++) {
+ 	console.log("names of American Presidents:", arr[i]);
+ }
+
+var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+
+var antSpecies = {};
+var arr = antSpecies[i];
+
+
+for (var i = 0; i < antSpecies.length; i++) {
+
+	console.log("names of the antSpecies", arr);
+}
+
+console.log("hello")
+
